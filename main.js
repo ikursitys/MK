43147845eb6ad99e14ae9188af2e349a9b33ec88
ikursitys <64460178+ikursitys@users.noreply.github.com>
2021-10-14 17:53:47 +0300

    HomeWork-2-fixed
    Исправила. Спасибо за фидбэк! :)
    Домашка класс, было сложно, но я разобралась :) Жду следующий урок!

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//Task 0
 const player1 = {
   name: "SCORPION",
   hp: 100,
@@ -19,42 +18,7 @@ const player2 = {
   },
 };
 
-//Task 1
-
-// const createPlayer = (pClass, pName, hp) => {
-//   const player = document.createElement("div");
-//   const progressBar = document.createElement("div");
-//   const character = document.createElement("div");
-//   const life = document.createElement("div");
-//   const name = document.createElement("div");
-//   const img = document.createElement("img");
-//   const arenas = document.querySelector("div.arenas");
-
-//   player.classList.add(pClass);
-//   progressBar.classList.add("progressbar");
-//   character.classList.add("character");
-//   life.classList.add("life");
-//   name.classList.add("name");
-
-//   life.style.width = hp + "%";
-//   name.innerText = pName;
-
-//   player.appendChild(progressBar);
-//   player.appendChild(character);
-//   progressBar.appendChild(life);
-//   progressBar.appendChild(name);
-//   character.appendChild(img);
-//   arenas.appendChild(player);
-
-//   img.src = "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif";
-// };
-
-//Task 2
-
-// createPlayer("player1", "SCOPRPION", 100);
-// createPlayer("player2", "SCORPION", 80);
-
-// Task 3
+const arenas = document.querySelector("div.arenas");
 
 const createPlayer = (pClass, object) => {
   const player = document.createElement("div");
@@ -79,10 +43,10 @@ const createPlayer = (pClass, object) => {
   progressBar.appendChild(life);
   progressBar.appendChild(name);
   character.appendChild(img);
-  arenas.appendChild(player);
 
   img.src = object.img;
+  return player;
 };
 
-createPlayer("player1", player1);
-createPlayer("player2", player2);
+arenas.appendChild(createPlayer("player1", player1));
+arenas.appendChild(createPlayer("player2", player2));
